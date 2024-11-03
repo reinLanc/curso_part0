85f6034713370f329b50b96bd7ecdc14f2ef47d2
reinLanc <reinaldogilmadrid@gmail.com>
2024-11-03 15:25:28 +0100

    parte1 courseinfo

---
+++ courseinfo/src/App.jsx
@@ -0,0 +1,54 @@
+const Header = ({ course }) => <h1>{course}</h1>;
+
+const Part = ({ name, exercises }) => {
+  return (
+    <p>
+      {name} - {exercises}
+    </p>
+  );
+};
+
+const Content = ({ parts }) => {
+  return (
+    <div>
+      {parts.map((part, index) => (
+        <Part key={index} name={part.name} exercises={part.exercises} />
+      ))}
+    </div>
+  );
+};
+
+const Total = ({ parts }) => {
+  const total = parts?.reduce((sum, part) => sum + part.exercises, 0);
+  return <p>Total: {total}</p>;
+};
+
+const App = () => {
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
+
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  );
+};
+
+export default App;
