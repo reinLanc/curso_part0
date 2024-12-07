2aae38b8fb1f3822f9bfccae9b7be9b5c99be987
reinLanc <reinaldogilmadrid@gmail.com>
2024-12-07 16:29:51 +0100

    ejercicio 2-5

--- part2/part2/ejercicios2-1-2-5/src/components/Course.jsx
+++ part2/part2/ejercicios2-1-2-5/src/components/Course.jsx
@@ -1,13 +1,46 @@
-import Header from './Header';
-import Content from './Content';
+import React from 'react';
+
+const Header = ({ title }) => {
+  return <h1>{title}</h1>;
+};
+
+const Part = ({ name, exercises }) => {
+  return (
+    <p>
+      {name} {exercises}
+    </p>
+  );
+};
+
+const Content = ({ parts }) => {
+  return (
+    <div>
+      {parts.map((part) => (
+        <Part key={part.id} name={part.name} exercises={part.exercises} />
+      ))}
+    </div>
+  );
+};
+
+const Total = ({ total }) => {
+  return (
+    <p>
+      <strong>Total of exercises: {total}</strong>
+    </p>
+  );
+};
 
 const Course = ({ course }) => {
-    return (
-        <div>
-            <Header title={course.name} />
-            <Content parts={course.parts} />
-        </div>
-    )
-}
+  const totalExercises = course.parts.reduce((sum, part) => sum + part.exercises, 0);
+
+  return (
+    <div>
+      <Header title={course.name} />
+      <Content parts={course.parts} />
+      <Total total={totalExercises} />
+    </div>
+  );
+};
 
 export default Course;
+
