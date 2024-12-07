22f50ff082ffa2f20599e18b46c0040d54804f52
reinLanc <reinaldogilmadrid@gmail.com>
2024-12-07 16:17:34 +0100

    ejercicio 2-3

--- part2/part2/ejercicios2-1-2-5/src/components/Course.jsx
+++ part2/part2/ejercicios2-1-2-5/src/components/Course.jsx
@@ -1,16 +1,21 @@
+import React from 'react';
 import Header from './Header';
 import Content from './Content';
 import Total from './Total';
 
 const Course = ({ course }) => {
-    const totalExercises = course.parts.reduce((sum, part) => sum + part.exercises, 0);
-    return (
-        <div>
-            <Header title={course.name} />
-            <Content parts={course.parts} />
-            <Total total={totalExercises} />
-        </div>
-    )
-}
+    const totalExercises = course.parts.reduce((sum, part) => {
+        console.log('Current sum:', sum, 'Current part:', part);
+        return sum + part.exercises;
+    }, 0);      
+
+  return (
+    <div>
+      <Header title={course.name} />
+      <Content parts={course.parts} />
+      <Total total={totalExercises} />
+    </div>
+  );
+};
 
 export default Course;
