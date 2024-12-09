cb9533ad6122ab173043063e227b98cbcaa3ffc1
reinLanc <reinaldogilmadrid@gmail.com>
2024-12-09 13:23:58 +0100

    ejercicio 2-15 rev

--- part2/ejercicios2-12-2-15/src/components/Persons.jsx
+++ part2/ejercicios2-12-2-15/src/components/Persons.jsx
@@ -1,7 +1,7 @@
 const Persons = ({ persons, deletePerson }) => (
   <ul>
     {persons.map(person => (
-      <li key={person.id}>
+      <li key={person.name}>
         {person.name} - {person.number}
         <button onClick={() => deletePerson(person.id)}>delete</button>
       </li>
