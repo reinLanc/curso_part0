ab07749424115713ca8c0ea668391629d610bc41
reinLanc <reinaldogilmadrid@gmail.com>
2024-12-17 22:07:28 +0100

    ejercicio 5-7

--- parte5/bloglist-frontend/src/components/Blog.jsx
+++ parte5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,40 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [detailsVisible, setDetailsVisible] = useState(false)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const toogleDetails = () => {
+    setDetailsVisible(!detailsVisible)
+  }
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title} - {blog.author}
+        <button onClick={toogleDetails}>
+          {detailsVisible ? 'Hide' : 'View'}
+        </button>
+      </div>
+      {detailsVisible && (
+        <div>
+          <p>{blog.url}</p>
+          <p>
+            Likes: {blog.likes}
+            <button>Like</button>
+          </p>
+          <p>{blog.user.name}</p>
+        </div>
+      )}
+    </div>
+  )
+}
 
 export default Blog
