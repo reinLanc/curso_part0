2ec022c61d366cb7c322b1daa0551dc8c2f7834a
reinLanc <reinaldogilmadrid@gmail.com>
2024-12-19 17:06:26 +0100

    ejercicio 5-14

--- parte5/bloglist-frontend/src/components/Blog.test.jsx
+++ parte5/bloglist-frontend/src/components/Blog.test.jsx
@@ -1,6 +1,7 @@
 import { render, screen } from '@testing-library/react'
 import Blog from './Blog'
 import { expect } from 'vitest'
+import userEvent from '@testing-library/user-event'
 
 test('renders content', () => {
   const blog = {
@@ -14,7 +15,9 @@ test('renders content', () => {
       }
     ]
   }
-  render(<Blog blog={blog} updateBlog={() => {}} deleteBlog={() => {}}/>)
+
+
+  render(<Blog blog={blog} updateBlog={() => {}} deleteBlog={() => {}} username="Test User" />)
 
   const title = screen.getByText('Bono postman 222 - Bono Vox')
   expect(title).toBeDefined()
@@ -23,4 +26,25 @@ test('renders content', () => {
   const likes = screen.queryByText('Likes: 10703')
   expect(url).toBeNull()
   expect(likes).toBeNull()
+})
+
+test('shows URL and likes when "view" button is clicked', async () => {
+  const user = userEvent.setup()
+  const blog = {
+    title: 'Bono postman 222',
+    author: 'Bono Vox',
+    url:'http://JWT.com',
+    likes:10703,
+    user:[
+      {
+        name:'Test User'
+      }
+    ]
+  }
+  render(<Blog blog={blog} updateBlog={() => {}} deleteBlog={() => {}} username="Test User" />)
+  const button = screen.getByText('View')
+  await user.click(button)
+
+  expect(screen.getByText('http://JWT.com')).toBeDefined()
+  expect(screen.getByText('Likes: 10703')).toBeDefined()
 })
