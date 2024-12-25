cf39dff67b824b41bc275603c54b36fa062f8b9a
reinLanc <reinaldogilmadrid@gmail.com>
2024-12-25 17:41:10 +0100

    ejercicio 6-14

--- part6/redux-anecdotes/src/App.jsx
+++ part6/redux-anecdotes/src/App.jsx
@@ -3,6 +3,7 @@ import AnecdoteList from './components/AnecdoteList'
 import Filter from './components/Filter'
 import Notification from './components/Notification'
 
+
 const App = () => {
   return (
     <div>
--- part6/redux-anecdotes/src/components/AnecdoteList.jsx
+++ part6/redux-anecdotes/src/components/AnecdoteList.jsx
@@ -1,24 +1,36 @@
 import { useDispatch, useSelector } from 'react-redux'
-import { voteAnecdote } from '../reducers/anecdoteReducer'
+import { useEffect, useMemo } from 'react'
+import anecdoteService from '../services/anecdotes'
+import { setAnecdotes, voteAnecdote } from '../reducers/anecdoteReducer'
 import { clearNotification, setNotification } from '../reducers/notificationReducer'
 
 const AnecdoteList = () => {
   const dispatch = useDispatch()
-  const anecdotes = useSelector((state) => state.anecdotes.anecdotes || [])
+  const anecdotes = useSelector((state) => state.anecdotes || [])
   const filter = useSelector((state) => state.filter || '')
 
+  useEffect(() => {
+    anecdoteService.getAll().then((anecdotes) => {
+      dispatch(setAnecdotes(anecdotes))
+    })
+  }, [dispatch])
+
+  const orderedAnecdotes = useMemo(() => {
+    return anecdotes
+      .filter((anecdote) =>
+        anecdote.content.toLowerCase().includes(filter.toLowerCase())
+      )
+      .sort((a, b) => b.votes - a.votes)
+  }, [anecdotes, filter])
+
   const vote = (id) => {
-    const anecdote = anecdotes.find(a => a.id === id)
+    const anecdote = anecdotes.find((a) => a.id === id)
     dispatch(voteAnecdote(id))
     dispatch(setNotification(`You have voted: "${anecdote.content}"`))
     setTimeout(() => {
       dispatch(clearNotification())
     }, 5000)
   }
-
-  const orderedAnecdotes = [...anecdotes]
-    .filter((anecdote) => anecdote.content.toLowerCase().includes(filter.toLowerCase()))
-    .sort((a, b) => b.votes - a.votes)
 
   return (
     <div>
