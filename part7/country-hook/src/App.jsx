8f4e1d039d0061349df7daa0a05355bf561569a3
reinLanc <reinaldogilmadrid@gmail.com>
2024-12-27 20:37:26 +0100

    ejercicio 7-7 hook

--- part7/country-hook/src/App.jsx
+++ part7/country-hook/src/App.jsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react'
-import useCountry from './hooks'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const useField = (type) => {
   const [value, setValue] = useState('')
@@ -11,37 +11,62 @@ const useField = (type) => {
   return {
     type,
     value,
-    onChange,
+    onChange
   }
-}
-
-const Country = ({ country }) => {
-  if (!country) {
-    return null
-  }
-
-  if (!country.found) {
-    return <div>Not found...</div>
-  }
-
-  return (
-    <div>
-      <h3>{country.data.name.common}</h3>
-      <div>Capital: {country.data.capital}</div>
-      <div>Population: {country.data.population}</div>
-      <img src={country.data.flags.png} height='100' alt={`flag of ${country.data.name.common}`} />
-    </div>
-  )
 }
 
 const App = () => {
   const nameInput = useField('text')
   const [name, setName] = useState('')
-  const country = useCountry(name)
+  const [country, setCountry] = useState(null)
+
+  useEffect(() => {
+    if (!name.trim()) {
+      setCountry(null)
+      return
+    }
+
+    const fetchCountry = async () => {
+      try {
+        const response = await axios.get(
+          `https://studies.cs.helsinki.fi/restcountries/api/name/${name}`
+        )
+        setCountry({ found: true, data: response.data })
+      } catch (error) {
+        console.error('Error fetching country data:', error.message)
+        setCountry({ found: false })
+      }
+    }
+
+    fetchCountry()
+  }, [name])
 
   const fetch = (e) => {
     e.preventDefault()
     setName(nameInput.value.trim())
+  }
+
+  const Country = ({ country }) => {
+    if (!country) {
+      return null
+    }
+
+    if (!country.found) {
+      return (
+        <div>
+          Not found...
+        </div>
+      )
+    }
+
+    return (
+      <div>
+        <h3>{country.data.name.common}</h3>
+        <div>Capital: {country.data.capital}</div>
+        <div>Population: {country.data.population}</div>
+        <img src={country.data.flags.png} height='100' alt={`flag of ${country.data.name.common}`} />
+      </div>
+    )
   }
 
   return (
