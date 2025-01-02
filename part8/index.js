5cd22e2842fe69931e9726fc7434b2f0f705f137
reinLanc <reinaldogilmadrid@gmail.com>
2025-01-02 15:45:02 +0100

    ejercicio 8-4 & 8-5

--- part8/index.js
+++ part8/index.js
@@ -112,7 +112,7 @@ const typeDefs = `
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks: [Book!]!
+    allBooks(author: String, genre: String): [Book!]!
     allAuthors: [Author!]!
   }
 `
@@ -121,7 +121,17 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books,
+    allBooks: (_,args) => {
+      let filteredBooks = books
+
+      if(args.author) {
+        filteredBooks = filteredBooks.filter((book) => book.author === args.author )
+      }
+      if(args.genre) {
+        filteredBooks = filteredBooks.filter((book) => book.genres.includes(args.genre))
+      }
+      return filteredBooks
+    },
     allAuthors: () =>
       authors.map((author) => ({
         ...author,
