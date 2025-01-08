ec3f429a770aa847f7041169683df0056e197bab
reinLanc <reinaldogilmadrid@gmail.com>
2025-01-08 03:08:35 +0100

    ejercicio 9-9

--- part9/patientor-backend/src/index.ts
+++ part9/patientor-backend/src/index.ts
@@ -1,9 +1,18 @@
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
+
 app.use(express.json());
 
-app.get('/api/ping',(_req, res) => {
+app.use(cors({
+    origin: 'http://localhost:5173',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+}));
+
+app.get('/api/ping', (_req, res) => {
     console.log('Ping request received');
     res.send('Pong');
 });
